051c4c08357aaeb2cab2bde3fed37f4ff4cd0afb
achingbrain <alex@achingbrain.net>
2018-01-22 17:25:19 +0000

    Adding payment lambda

--- packages/lambda-send-payment/index.js
+++ packages/lambda-send-payment/index.js
@@ -1,11 +1,35 @@
+var stripe = require('stripe')(process.env.STRIPE_KEY)
 
-var stripe = require("stripe")(process.env.STRIPE_KEY)
-
-exports.handler = (event, context, callback) => {  
+exports.handler = (event, context, callback) => {
   stripe.charges.create({
     amount: event.amount,
     currency: event.currency,
     description: event.description,
-    source: event.token,
-  }, callback)
+    source: event.token
+  }, (error, result) => {
+    var statusCode = 200
+    var responseBody = {}
+
+    if (error) {
+      statusCode = 500
+
+      if (error.statusCode === 401) {
+        statusCode = 502
+      }
+
+      responseBody.message = error.message
+      responseBody.stack = error.stack
+    } else {
+      responseBody.result = result
+    }
+
+    var response = {
+      statusCode: statusCode,
+      headers: {},
+      body: JSON.stringify(responseBody),
+      isBase64Encoded: false
+    }
+
+    callback(null, response)
+  })
 }
