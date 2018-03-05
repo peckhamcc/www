dbdc15b11a0b75a0035e549a4ea3c3eb58f2bbf0
achingbrain <alex@achingbrain.net>
2018-03-05 18:30:55 +0000

    Add mobile meta tag

--- packages/website/src/components/slide-show.js
+++ packages/website/src/components/slide-show.js
@@ -2,9 +2,29 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import styled, { keyframes } from 'styled-components'
 
-const transition = keyframes`
-  16.665%, 33.33% {opacity: 1;}
-  49.995%, 100% {opacity: 0;}`;
+const FADE_TIME = 50
+
+const fadeOut = (props) => {
+  const t = 100 / props.total
+
+  return keyframes`
+    0% {
+      opacity: 1;
+    }
+
+    ${(t * (props.total / 10)) * 1.2}% {
+      opacity: 1;
+    }
+
+    ${t * 1.2}% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 0;
+    }
+  `
+}
 
 const Container = styled.ul`
   list-style: none;
@@ -12,24 +32,24 @@ const Container = styled.ul`
   height: 100vh;
   padding: 0;
   margin: 0;
-`;
+`
 
 const Slide = styled.li`
   position: absolute;
   opacity: 0;
   height: 100vh;
   width: 100%;
+  z-index: ${props => 100 - props.order};
   background-image: ${props => 'url("' + props.background + '")' };
   background-size: cover;
   background-position: center top;
-  animation: 18s ${transition} linear infinite 0s; /* 6 second per slide, 3 slides, total 18s */
-  animation-delay: ${props => props.order * 6 + 's'}; /* use the order prop as a multiplicator to have each slide appear/disappear successively */
-`;
+  animation: ${fadeOut} ${FADE_TIME}s ${props => props.order * (FADE_TIME * (1 / props.total))}s linear infinite;
+`
 
 const SlideShowWrapper = ({ slides }) => {
   return (
     <Container>
-        {slides.map((background, key) => <Slide background={background} key={key} order={key} />)}
+        {slides.map((background, key, array) => <Slide background={background} key={key} order={key} total={array.length} />)}
     </Container>
   )
 }
