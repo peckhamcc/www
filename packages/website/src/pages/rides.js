04187f29df3a9093d481ca1288bae17133d8085d
achingbrain <alex@achingbrain.net>
2018-04-23 17:13:37 +0100

    Update rides page content

--- packages/website/src/pages/rides.js
+++ packages/website/src/pages/rides.js
@@ -13,7 +13,7 @@ class RidesPage extends Component {
         <Hero background={ridesBackground.src} />
         <Panel>
           <h2>Club Rides</h2>
-          <p>All rides leave <a href="http://www.southwark.gov.uk/libraries/find-a-library?chapter=13">Peckham Library</a> at 8am or 8:30am depending on the time of year.</p>
+          <p>All rides leave <a href="http://www.southwark.gov.uk/libraries/find-a-library?chapter=13">Peckham Library</a> at 8am in the summer or 8:30am in the winter.</p>
           <p>Details of weekend rides are usually posted to <a href="https://www.facebook.com/PeckhamCC">Facebook</a> and <a href="https://twitter.com/peckhamcc">Twitter</a> towards the end of the week.</p>
           <p>First time riders are more than welcome but if you are thinking of joining us for the first time, please get in touch first so we know to look out for you!</p>
           <h3>Social Spin</h3>
@@ -21,10 +21,10 @@ class RidesPage extends Component {
           <p>It's a non-drop ride, meaning we regroup at the tops of climbs and everyone rides back together.</p>
           <p>The '<Link to='/routes'>short loop</Link>' is about 60km long and takes about three hours.  At the 30km point we usually decide whether to go further, sometimes splitting the group if people have to be back early.</p>
           <h3>Ladies Ride</h3>
-          <p>Once a month there is a women-only ride aimed at riders of all abilities.</p>
+          <p>On the third Sunday of every month there is a women-only ride aimed at riders of all abilities.  The route varies month to month but is about 50km in length and the group leaves the library at 9am.</p>
           <h3>Training Rides</h3>
           <p>Depending on the time of year and people's training goals, there my be a faster group on Saturdays and sometimes Sundays too.</p>
-          <p>These rides can be up to 200km in length.</p>
+          <p>These rides are around 150-200km in length.</p>
         </Panel>
         <LinkPanel background={equipmentBackground.src}>
           <Link to='/equipment'>Equipment</Link>
