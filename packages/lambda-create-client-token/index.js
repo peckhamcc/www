e244395dfb7550de812672ca74d2eeeb9ea33196
achingbrain <alex@achingbrain.net>
2018-05-29 10:45:18 +0100

    Simply client token generation

--- packages/lambda-create-client-token/index.js
+++ packages/lambda-create-client-token/index.js
@@ -1,48 +1,36 @@
-var braintree = require('braintree')
-var gateway = braintree.connect({
+const middy = require('middy')
+const {
+  httpErrorHandler,
+  httpHeaderNormalizer,
+  cors
+} = require('middy/middlewares')
+const braintree = require('braintree')
+const gateway = braintree.connect({
   environment: braintree.Environment[process.env.BT_ENVIRONMENT],
   merchantId: process.env.BT_MERCHANT_ID,
   publicKey: process.env.BT_PUBLIC_KEY,
   privateKey: process.env.BT_PRIVATE_KEY
 })
 
-const allowedOrigins = [
-  'https://dev.peckham.cc',
-  'https://www.peckham.cc',
-  'https://peckham.cc'
-]
+const createClientToken = (body, context, callback) => {
+  gateway.clientToken.generate({}, (error, result) => {
+    if (error) {
+      return callback(error)
+    }
 
-const respond = (statusCode, event, response, callback) => {
-  let allowOrigin = 'null'
-
-  if (event && event.headers && allowedOrigins.includes(event.headers.origin)) {
-    allowOrigin = event.headers.origin
-  }
-
-  callback(null, {
-    statusCode: statusCode,
-    headers: {
-      'Access-Control-Allow-Origin': allowOrigin,
-      'Access-Control-Allow-Credentials': true
-    },
-    body: response ? JSON.stringify(response, null, 2) : '',
-    isBase64Encoded: false
+    callback(null, {
+      clientToken: result.clientToken
+    })
   })
 }
 
-exports.handler = (event, context, callback) => {
-  gateway.clientToken.generate({}, (error, result) => {
-    var statusCode = 200
-    var responseBody = {}
+const handler = middy(createClientToken)
+  .use(cors({
+    origin: process.env.NODE_ENV !== 'development' ? 'https://peckham.cc' : '*'
+  }))
+  .use(httpHeaderNormalizer())
+  .use(httpErrorHandler())
 
-    if (error) {
-      statusCode = 500
-
-      console.error(error)
-    } else {
-      responseBody.clientToken = result.clientToken
-    }
-
-    respond(statusCode, event, responseBody, callback)
-  })
+module.exports = {
+  handler
 }
