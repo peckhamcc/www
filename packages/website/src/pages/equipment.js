8afba855fba471796b818b2ae250963f9a22b9b9
achingbrain <alex@achingbrain.net>
2018-09-08 23:31:24 +0100

    Update equipment

--- packages/website/src/pages/equipment.js
+++ packages/website/src/pages/equipment.js
@@ -18,12 +18,12 @@ class EquipmentPage extends Component {
             <li>Helmet - saftey first</li>
             <li>Road Bike - it doesn't have to be expensive but it should have drop handlebars and gears</li>
             <li>Pedals - clip-in (or clipless) pedals and road-specific shoes are a must - they are more efficient than flats &amp; trainers and safter than toe-clips</li>
-            <li>Puncture repair kit - 2x spare inner tubes, tyre levers and a small pump or CO<sub>2</sub> cannister</li>
+            <li>2x spare inner tubes, tyre levers and a small pump or CO<sub>2</sub> cannister</li>
             <li>Multi-tool - these are usually small and have the basic tools for fixing common bike problems</li>
             <li>Lights - when riding in darker months it's good to see and be seen</li>
             <li>Water bottle &amp; bottle cages - it's important to stay hydrated especially in warmer weather</li>
             <li>Food - energy bars, gels, bananas, etc. Try to eat something every 20kms or so - small and often is key to keeping your energy up.</li>
-            <li>Clothing - we all look a bit silly in lycra but road-specific cycling jerseys and bib shorts are essential for your comfort on the bike and have pockets that can hold snacks &amp; your keys/phone and all the bits you'll need on a ride. Light weight rain jackets or gilets are a good all round choice and can pack down into a jersey pocket when not in use.</li>
+            <li>Clothing - road-specific cycling jerseys and bib shorts are essential for your comfort on the bike and have pockets that can hold snacks &amp; your keys/phone etc. Light weight rain jackets or gilets are a good all round choice and can pack down into a jersey pocket when not in use.</li>
             <li>Mobile phone - in case of emergency.  Selfie emergencies.</li>
             <li>A small amount of cash. Cards are good too but some coffee stops in the countryside only take cash.</li>
             <li>Glasses - these will keep sun/wind/rain &amp; road grit out of your eyes</li>
