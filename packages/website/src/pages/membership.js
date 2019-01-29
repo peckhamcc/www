acbb3b90851dcf5599c63785b6dac1680fcf3c1d
achingbrain <alex@achingbrain.net>
2019-01-29 10:24:42 +0000

    update membership page to include fee

--- packages/website/src/pages/membership.js
+++ packages/website/src/pages/membership.js
@@ -13,12 +13,11 @@ class MembershipPage extends Component {
         <Hero background={membershipBackground.src} />
         <Panel>
           <h2>Membership</h2>
-          <p>There are no membership fees or club dues for riding with Peckham Cycle Club, just turn up and ride.</p>
-          <p>We hope to keep the club fee free forever but we do have some overheads so to support the club a small levy is added to all club kit orders which pays for things like <a href='https://www.britishcycling.org.uk/club/profile/8092/peckham-cycle-club'>British Cycling Affiliation</a>.</p>
-          <p>Organisation takes place primarily via a <a href="https://www.whatsapp.com/">WhatsApp</a> group, please get in touch on <a href="https://www.facebook.com/PeckhamCC">Facebook</a> or <a href="https://twitter.com/peckhamcc">Twitter</a> if you'd like to be added to the group.</p>
-          <p>We also have a group page on <a href="https://www.strava.com/clubs/63491">Strava</a>.</p>
-          <h3>British Cycling Membership</h3>
-          <p>You don't have to be a member of British Cycling to ride with us but we do recommend it.</p>
+          <p>Our weekly social rides are free to join and will be forever.</p>
+          <p>If you've been a couple of times and you like the club and wish to continue riding with us, there is a yearly fee currently set at £25 (with a £1 admin fee).</p>
+          <p>Paying this fee will allow you to purchase club kit and join us on training events and trips abroad. It is payable once a year and is non-refundable.</p>
+          <p>You can pay the membership fee online via our profile page on the <a href="https://www.britishcycling.org.uk/club/profile/8092/peckham-cycle-club">British Cycling website</a>.</p>
+          <p>British Cycling membership is advised but is not a requirement.</p>
         </Panel>
         <LinkPanel background={ridesBackground.src}>
           <Link to='/rides'>Rides</Link>
