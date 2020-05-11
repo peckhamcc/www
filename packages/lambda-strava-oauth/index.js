54204d32f28a30e0a6d687996d4d6f18f78b8a5b
achingbrain <alex@achingbrain.net>
2020-05-11 12:56:12 +0100

    add oauth lambda

---
+++ packages/lambda-strava-oauth/index.js
@@ -0,0 +1,119 @@
+const middy = require('middy')
+const { HttpError } = require('http-errors')
+const {
+  jsonBodyParser,
+  validator,
+  httpHeaderNormalizer,
+  cors
+} = require('middy/middlewares')
+const https = require('https')
+const querystring = require('querystring')
+const AWS = require('aws-sdk')
+
+async function sendCode (code) {
+  return new Promise((resolve, reject) => {
+    const postData = querystring.stringify({
+      client_id: process.env.STRAVA_CLIENT_ID,
+      client_secret: process.env.STRAVA_CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      code
+    })
+
+    const options = {
+      hostname: 'www.strava.com',
+      path: '/api/v3/oauth/token',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(postData)
+      }
+    }
+
+    const req = https.request(options, (res) => {
+      res.setEncoding('utf8')
+      let body = ''
+      res.on('data', (chunk) => {
+        body += chunk
+      })
+      res.on('end', () => {
+        resolve(JSON.parse(body))
+      })
+    })
+
+    req.on('error', err => {
+      reject(err)
+    })
+
+    // Write data to request body
+    req.write(postData)
+    req.end()
+  })
+}
+
+async function exchangeCode ({ query: { code } }) {
+  const result = await sendCode(code)
+
+  const db = new AWS.DynamoDB({
+    apiVersion: config.aws.ses.version,
+    region: config.aws.ses.region
+  })
+  const docClient = new AWS.DynamoDB.DocumentClient()
+
+  await docClient.put({
+    TableName: process.env.AWS_DB_TABLE,
+    Item: {
+      id: result.athlete.id,
+      ...result
+    }
+  })
+  .promise()
+}
+
+const inputSchema = {
+  type: 'object',
+  properties: {
+    query: {
+      type: 'object',
+      properties: {
+        code: {
+          type: 'string'
+        }
+      },
+      required: ['code']
+    }
+  }
+}
+
+const errorHandler = () => ({
+  onError: (handler, next) => {
+    if (handler.error instanceof HttpError) {
+      if (handler.error.message.includes('failed validation')) {
+        const details = handler.error.details[0]
+
+        handler.response = {
+          statusCode: 422,
+          body: JSON.stringify({
+            field: details.dataPath.replace('.body.', '')
+          })
+        }
+
+        next()
+      } else {
+        next(handler.error)
+      }
+    } else {
+      next(handler.error)
+    }
+  }
+})
+
+module.exports = {
+  handler: middy(exchangeCode)
+    .use(httpHeaderNormalizer())
+    .use(jsonBodyParser())
+    .use(validator({ inputSchema }))
+    .use(errorHandler())
+    .use(cors({
+      origin: process.env.NODE_ENV !== 'development' ? 'https://peckham.cc' : '*'
+    }))
+}
