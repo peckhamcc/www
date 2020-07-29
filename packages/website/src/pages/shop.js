a9f04c43a243693c83c5b329d1a83893d861e2c1
Leo Hemsted <leohemsted@gmail.com>
2020-07-29 06:59:51 +0100

    add preamble to shop (#20)
    We get an email every month or two from someone who isn't a friend asking for the shop code. Let's just put a little preamble explaining this rather than writing it from scratch in a new email each time.

--- packages/website/src/pages/shop.js
+++ packages/website/src/pages/shop.js
@@ -2,6 +2,9 @@ import React, {
   Component
 } from 'react'
 import {
+  Link
+} from 'react-router-dom'
+import {
   PageWrapper,
   Panel,
   ShopListItem,
@@ -22,6 +25,16 @@ class ShopPage extends Component {
     return (
       <PageWrapper>
         <Hero background={shopBackground.src} />
+        <Panel>
+          <p>
+            Our Kit is made to order, and is only availabe to <Link to='/membership'>friends of Peckham CC</Link>, our
+            voluntary membership scheme.
+          </p>
+          <p>
+            However, everyone is welcome to buy our musettes (tote bags), bidons (bottles) or casquettes (bike caps)
+            from <a href='https://ratracecycles.com/'>Rat Race Cycles</a>.
+          </p>
+        </Panel>
         <Panel>
           <h2>Shop</h2>
           <Items>
