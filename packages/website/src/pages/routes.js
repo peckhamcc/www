c2aa257957a45471d2a31b08a9817d0451b75ad4
Leo Hemsted <leohemsted@gmail.com>
2020-11-05 18:17:28 +0000

    move routes to ridewithgps collection
    The routes page was a bit neglected, and the strava routes and embedded
    gpx files had diverged. In a bid to make the routes page easier to
    maintain, move from storing the files locally to a ridewithgps
    collection. Currently this collection is tied to Leo's ridewithgps
    account, but we could move to a club account if we wanted.
    
    You can add any ridewithgps route to collections, but to ensure that
    routes don't disappear I recommend any routes we want to add we make a
    copy and use that, and then pledge not to edit it again.

--- packages/website/src/pages/routes.js
+++ packages/website/src/pages/routes.js
@@ -8,32 +8,15 @@ import {
   Hero
 } from '../components/panels'
 import routesBackground from '../../assets/routes-bg.jpg'
-import socialLoopGpx from '../../assets/peckham-cc-social-loop.gpx'
-import knattsValleyGpx from '../../assets/peckham-cc-knatts-valley.gpx'
-import surreyHillsGpx from '../../assets/peckham-cc-surrey-hills.gpx'
 import styled from 'styled-components'
 import {
-  FaRegMap,
-  FaChevronCircleUp
+  FaRegMap
 } from 'react-icons/fa'
 import {
   light
 } from '../colours'
 
-const Map = styled.iframe`
-  width: 500px;
-  height: 400px;
-  margin-top: 20px;
-`
-
-const SharingList = styled.ul`
-  margin: 0 0 20px 10px;
-  padding: 0;
-`
-
-const Sharing = styled.li`
-  list-style: none;
-
+const Sharing = styled.p`
   a {
     text-decoration: none;
     color: ${light};
@@ -43,63 +26,42 @@ const Sharing = styled.li`
     text-decoration: underline;
   }
 `
-
-const routes = [{
-  hash: 'the-social-loop',
-  title: 'The Social Loop',
-  description: 'A staple of our club Social Spin this 60km/770m route takes is a great introduction to the club.',
-  gpx: socialLoopGpx,
-  strava: 'https://www.strava.com/routes/23414432',
-  embed: 'https://www.plotaroute.com/embedmap/1016515'
-}, {
-  hash: 'knatts-valley',
-  title: 'Knatts Valley',
-  description: 'A touch short of 90km and with 1175m of climbing this is mostly quiet country lanes.',
-  gpx: knattsValleyGpx,
-  strava: 'https://www.strava.com/routes/11036595',
-  embed: 'https://www.plotaroute.com/embedmap/550078'
-}, {
-  hash: 'surrey-hills',
-  title: 'Surrey Hills',
-  description: 'A tougher training ride, this 145km/1800m route takes in the major climbs of the Ride London 100 mile sportive.',
-  gpx: surreyHillsGpx,
-  strava: 'https://www.strava.com/routes/14040879',
-  embed: 'https://www.plotaroute.com/embedmap/655505'
-}]
-
 class RoutesPage extends Component {
   render () {
     return (
       <PageWrapper>
         <Hero background={routesBackground.src} />
         <Panel>
-          <h2>Club Routes</h2>
-          <p>These are some routes that we commonly use.</p>
-          {
-            routes.map((route, index) => {
-              return (
-                <InnerPanel key={index}>
-                  <h3 id={route.hash}>{route.title}</h3>
-                  <SharingList>
-                    <Sharing><a href={route.gpx}><FaRegMap /> Download .gpx</a></Sharing>
-                    <Sharing><a href={route.strava}><FaChevronCircleUp /> View on Strava</a></Sharing>
-                  </SharingList>
-                  <p>{route.description}</p>
-                  <Map
-                    name={route.title}
-                    src={route.embed}
-                    frameborder='0'
-                    scrolling='no'
-                    allowfullscreen
-                    webkitallowfullscreen
-                    mozallowfullscreen
-                    oallowfullscreen
-                    msallowfullscreen
-                  />
-                </InnerPanel>
-              )
-            })
-          }
+          <InnerPanel>
+            <h2>Club Routes</h2>
+            <p>
+              Peckham CC has a variety of routes that we often draw from, ranging from 50-60km social rides
+              all the way up to hilly epics and seaside jaunts.
+            </p>
+            <p>
+              Feel free to grab routes from our RideWithGPS collection, and download GPX files to your phone or bike
+              computer to help you navigate on the go.
+            </p>
+            <Sharing>
+              <a href='https://ridewithgps.com/collections/15759?privacy_code=WobhC2AqkEQxBQvK'><FaRegMap /> View on RideWithGPS</a>
+            </Sharing>
+          </InnerPanel>
+          <InnerPanel>
+            <h2>Gravel/CX Routes</h2>
+            <p>
+              We've got a budding gravel/cx/muddy off road scene too! Our rides are often organised ad-hoc - keep an eye
+              on the facebook event or ask on our WhatsApp for more details about upcoming rides. The rides are
+              typically at a social pace, and often feature route changes on the fly and larking around in the woods.
+              Our gravel rides are welcome to anyone with chunky tyres who doesn't mind getting a bit muddy.
+            </p>
+            <p>
+              Feel free to grab routes from our RideWithGPS collection, and download GPX files to your phone or bike
+              computer to help you navigate on the go.
+            </p>
+            <Sharing>
+              <a href='https://ridewithgps.com/collections/15762?privacy_code=Fb0nGIH25lNd9yzG'><FaRegMap /> View on RideWithGPS</a>
+            </Sharing>
+          </InnerPanel>
         </Panel>
       </PageWrapper>
     )
