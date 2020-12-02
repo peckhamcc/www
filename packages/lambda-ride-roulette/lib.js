f3b9676e9bc67e4740a1433d4bdad4b1cf6db16e
achingbrain <alex@achingbrain.net>
2020-12-02 12:38:00 +0000

    fix timestamps for safari

--- packages/lambda-ride-roulette/lib.js
+++ packages/lambda-ride-roulette/lib.js
@@ -5,7 +5,7 @@ function nextDate (now, dayIndex) {
   var day = new Date(now.getTime())
   day.setDate(day.getDate() + (dayIndex - 1 - day.getDay() + 7) % 7 + 1)
 
-  return `${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`
+  return `${day.getFullYear()}-${(day.getMonth() + 1).toString().padStart(2, '0')}-${day.getDate().toString().padStart(2, '0')}`
 }
 
 function getNextRidingDays () {
