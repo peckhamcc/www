8c9cd5b9836d6ec37d479c30feaf800d5be9091e
achingbrain <alex@achingbrain.net>
2021-02-24 10:23:18 +0000

    mailer lite needs the target div on the page at load so use a popover instead

--- packages/website/src/pages/mailing-list.js
+++ packages/website/src/pages/mailing-list.js
@@ -4,17 +4,30 @@ import React, {
 import {
   PageWrapper,
   Panel,
+  Button,
   Hero
 } from '../components/panels'
 import background from '../../assets/mailing-list-bg.jpg'
 
 class MailingListPage extends Component {
+  handleSubscribe = (event) => {
+    event.preventDefault()
+
+    if (!this.ml) {
+      this.ml = window.ml('accounts', '2816293', 'n1z2e4e8x1', 'load')
+    }
+
+    this.ml('webforms', '3426454', 'l5f7t9', 'show')
+  }
+
   render () {
     return (
       <PageWrapper>
         <Hero background={background.src} />
         <Panel>
-          <div className='ml-form-embed' data-account='2816293:n1z2e4e8x1' data-form='3426454:l5f7t9' />
+          <h2>Mailing list</h2>
+          <p>Sign up to our mailing list for club updates, including rides, trips, training, kit, etc</p>
+          <Button onClick={this.handleSubscribe}>Subscribe</Button>
         </Panel>
       </PageWrapper>
     )
