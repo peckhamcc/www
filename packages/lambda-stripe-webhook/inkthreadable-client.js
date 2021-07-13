eb6e4a58123c62acdc921aaaba1245bb98e4d97c
achingbrain <alex@achingbrain.net>
2021-07-13 14:29:04 +0100

    change shipping method

--- packages/lambda-stripe-webhook/inkthreadable-client.js
+++ packages/lambda-stripe-webhook/inkthreadable-client.js
@@ -17,7 +17,7 @@ const PCC_ASSETS = 'http://cc.peckham.assets.s3-website.eu-west-2.amazonaws.com'
 
 const SHIPPING_LIMITS = [{
   max: 700,
-  method: 'recorded 24'
+  method: 'recorded'
 }, {
   max: Infinity,
   method: 'courier'
