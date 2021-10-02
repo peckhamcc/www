54a5f5affec20c44267875531e721327fd5c6bbf
achingbrain <alex@achingbrain.net>
2021-10-02 22:30:35 +0100

    chore: return members

--- packages/lambda-lib/members.js
+++ packages/lambda-lib/members.js
@@ -1,6 +1,18 @@
+const AWS = require('aws-sdk')
+const { config } = require('./config')
+
+AWS.config.update({
+  region: config.aws.dynamodb.region
+})
 
 async function getMembers () {
-  return []
+  const client = new AWS.DynamoDB.DocumentClient()
+
+  const items = await client.scan({
+    TableName: process.env.AWS_USERS_DB_TABLE
+  }).promise()
+
+  return items.Items
 }
 
 module.exports = {
