50e13ddbdc9f8d7dc917834a7e53a73c08c8e055
Alex Potsides <alex@achingbrain.net>
2022-05-22 13:49:28 +0100

    fix: fix homepage fade (#44)
    Rewrites homepage fade to handle fewer images properly.

--- packages/website/src/components/slide-show.jsx
+++ packages/website/src/components/slide-show.jsx
@@ -5,27 +5,38 @@ import styled, {
 import {
   MAX_PAGE_WIDTH
 } from '../units'
+import {
+  panelLevel3Background
+} from '../colours'
 
-const FADE_TIME = 50
+// https://www.devtwins.com/blog/css-cross-fading-images
+
+const VISIBLE_TIME = 2
+const CROSS_FADE_TIME = 1
+const ANIMATION_TIME = VISIBLE_TIME + CROSS_FADE_TIME
 
 const fadeOut = (props) => {
-  const t = 100 / props.total
+  const totalAnimationDuration = ANIMATION_TIME * props.total
 
   return keyframes`
     0% {
       opacity: 1;
     }
 
-    ${(t * (props.total / 10)) * 1.2}% {
+    ${(VISIBLE_TIME / totalAnimationDuration) * 100}% {
       opacity: 1;
     }
 
-    ${t * 1.2}% {
+    ${(1 / props.total) * 100}% {
+      opacity: 0;
+    }
+
+    ${100 - ((CROSS_FADE_TIME / totalAnimationDuration) * 100)}% {
       opacity: 0;
     }
 
     100% {
-      opacity: 0;
+      opacity: 1;
     }
   `
 }
@@ -36,24 +47,40 @@ const Container = styled.ul`
   height: 100vh;
   padding: 0;
   margin: 0;
+  background-color: ${panelLevel3Background};
+
+  ${props => {
+    let children = ''
+
+    for (let i = 0; i < props.total; i++) {
+      children += `
+  li:nth-of-type(${i + 1}) {
+    animation-delay: ${(props.total - i - 1) * ANIMATION_TIME}s;
+  }
+      `
+    }
+
+    return children
+  }}
 `
 
 const Slide = styled.li`
   position: absolute;
-  opacity: 0;
   height: 100vh;
   width: 100%;
   max-width: ${MAX_PAGE_WIDTH};
-  z-index: ${props => 100 - props.order};
   background-image: ${props => 'url("' + props.background + '")'};
   background-size: cover;
   background-position: center top;
-  animation: ${fadeOut} ${FADE_TIME}s ${props => props.order * (FADE_TIME * (1 / props.total))}s linear infinite;
+  animation-name: ${fadeOut};
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  animation-duration: ${props => `${ANIMATION_TIME * props.total}s`};
 `
 
 const SlideShowWrapper = ({ slides }) => {
   return (
-    <Container>
+    <Container total={slides.length}>
       {slides.map((background, key, array) => <Slide background={background} key={key} order={key} total={array.length} />)}
     </Container>
   )
