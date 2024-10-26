9876aefb98d36701c09c76e43669ddec8858d63d
achingbrain <alex@achingbrain.net>
2024-10-26 12:33:27 +0100

    feat: switch to new mailerlite

--- packages/website/src/pages/mailing-list.jsx
+++ packages/website/src/pages/mailing-list.jsx
@@ -4,30 +4,18 @@ import React, {
 import {
   PageWrapper,
   Panel,
-  Button,
   Hero
 } from '../components/panels'
 import background from '../../assets/mailing-list-bg.jpg'
 
 class MailingListPage extends Component {
-  handleSubscribe = (event) => {
-    event.preventDefault()
-
-    if (!this.ml) {
-      this.ml = window.ml('accounts', '2816293', 'n1z2e4e8x1', 'load')
-    }
-
-    this.ml('webforms', '3426454', 'l5f7t9', 'show')
-  }
-
   render () {
     return (
       <PageWrapper>
         <Hero background={background} />
         <Panel>
           <h2>Mailing list</h2>
-          <p>Sign up to our mailing list for club updates, including rides, trips, training, kit, etc</p>
-          <Button onClick={this.handleSubscribe}>Subscribe</Button>
+          <div className='ml-embedded' data-form='bRGP4R' />
         </Panel>
       </PageWrapper>
     )
